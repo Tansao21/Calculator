41bef7b211aa1d055958a96de7067a455870edc4
itpark <a.m.smyslov@yandex.ru>
2022-11-20 13:14:10 +0300

    Initial commit

---
+++ ConsoleAppMathCond/Program.cs
@@ -0,0 +1,38 @@
+﻿double firstNum = 0;
+double secondNum = 0;
+string action = "";
+
+Console.Write("Введите первое число: ");
+firstNum = double.Parse(Console.ReadLine());
+
+Console.Write("Введите действие(+ - * /): ");
+action = Console.ReadLine();
+
+Console.Write("Введите второе число: ");
+secondNum = double.Parse(Console.ReadLine());
+
+switch (action)
+{
+    case "+":
+        Console.WriteLine($"={firstNum + secondNum}");
+        break;
+    case "-":
+        Console.WriteLine($"={firstNum - secondNum}");
+        break;
+    case "*":
+        Console.WriteLine($"={firstNum * secondNum}");
+        break;
+    case "/":
+        if (secondNum == 0)
+        {
+            Console.WriteLine("Ошибка. На ноль делить нельзя");
+        }
+        else
+        {
+            Console.WriteLine($"={firstNum / secondNum}");
+        }
+        break;
+    default:
+        Console.WriteLine("Действие не распознано. Операция не может быть выполнена");
+        break;
+}

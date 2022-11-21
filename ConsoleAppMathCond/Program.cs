f0aafd92c1f65fbb629ef9c46d286dbdb22bbcd1
Tansao <tansao26@gmail.com>
2022-11-21 17:05:50 +0300

    Сalculator1

--- ConsoleAppMathCond/Program.cs
+++ ConsoleAppMathCond/Program.cs
@@ -1,6 +1,10 @@
 ﻿double firstNum = 0;
 double secondNum = 0;
 string action = "";
+string sinvol1 = "+";
+string sinvol2 = "-";
+string sinvol3 = "*";
+string sinvol4 = "/";
 
 Console.Write("Введите первое число: ");
 firstNum = double.Parse(Console.ReadLine());
@@ -14,22 +18,32 @@ secondNum = double.Parse(Console.ReadLine());
 switch (action)
 {
     case "+":
+        if (sinvol1 == "+")
+        {
         Console.WriteLine($"={firstNum + secondNum}");
+        }
         break;
     case "-":
-        Console.WriteLine($"={firstNum - secondNum}");
+        if (sinvol2 == "-")
+        {
+            Console.WriteLine($"={firstNum - secondNum}");
+        }
         break;
     case "*":
-        Console.WriteLine($"={firstNum * secondNum}");
+        if (sinvol3 == "*")
+        {
+
+            Console.WriteLine($"={firstNum * secondNum}");
+        }
         break;
     case "/":
-        if (secondNum == 0)
-        {
-            Console.WriteLine("Ошибка. На ноль делить нельзя");
-        }
-        else
+         if (sinvol4 == "/")
         {
             Console.WriteLine($"={firstNum / secondNum}");
+        }
+        else if(secondNum == 0)
+        {
+            Console.WriteLine("Ошибка. На ноль делить нельзя");
         }
         break;
     default:
